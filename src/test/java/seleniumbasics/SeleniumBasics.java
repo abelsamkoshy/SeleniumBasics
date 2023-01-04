03add9419fa8b847cddd123896078ae5aa30a65a
Abel Sam Koshy <abel.sam.koshy1947@gmail.com>
2023-01-04 07:43:13 +0530

    com

--- src/test/java/seleniumbasics/SeleniumBasics.java
+++ src/test/java/seleniumbasics/SeleniumBasics.java
@@ -1,19 +1,20 @@
 package seleniumbasics;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.Point;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 
 public class SeleniumBasics {
@@ -41,7 +42,7 @@ public class SeleniumBasics {
     @AfterMethod
     public void tearDown() {
        // driver.close();
-        //driver.quit();
+       // driver.quit();
     }
 
     @Test
@@ -281,4 +282,260 @@ public class SeleniumBasics {
         submit.submit();
 
     }
+    @Test
+    public void TC_017_verifyDiffBwFindElementAndFindElements(){
+        driver.get("https://selenium.obsqurazone.com/radio-button-demo.php");
+//        WebElement maleRadioButton = driver.findElement(By.id("inlineRadio11"));
+//        maleRadioButton.click();
+        List<WebElement> genders = driver.findElements(By.xpath("//input[@name='student-gender']"));
+        for(int i=0;i< genders.size();i++){
+            String gender= genders.get(i).getAttribute("value");
+            if(gender.equals("Male")){
+                genders.get(i).click();
+            }
+        }
+    }
+    @Test
+    public void TC_018_verifyMultipleWindowHandling() {
+        driver.get("https://demo.guru99.com/popup.php");
+        String parentWindow = driver.getWindowHandle();
+        System.out.println("parent window id = " + parentWindow);
+        WebElement clickLink = driver.findElement(By.xpath("//a[text()='Click Here']"));
+        clickLink.click();
+        Set<String> handles = driver.getWindowHandles();
+        System.out.println(handles);
+        Iterator<String> handleids = handles.iterator();
+        while (handleids.hasNext()) {
+            String childWindow = handleids.next();
+            if (!childWindow.equals(parentWindow)) {
+                driver.switchTo().window(childWindow);
+                WebElement emailField = driver.findElement(By.name("emailid"));
+                emailField.sendKeys("priya@gmail.com");
+                WebElement submitButton = driver.findElement(By.name("btnLogin"));
+                submitButton.click();
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentWindow);
+    }
+
+    @Test
+    public void TC_019_verifyMultipleWindowHandlingHomeWork1() {
+        driver.get("https://demoqa.com/browser-windows");
+        String parentWindow = driver.getWindowHandle();
+        WebElement newTabButton = driver.findElement(By.id("tabButton"));
+        newTabButton.click();
+        Set<String> handles = driver.getWindowHandles();
+        Iterator<String> handleids = handles.iterator();
+        while (handleids.hasNext()) {
+            String childWindow = handleids.next();
+            if (!childWindow.equals(parentWindow)) {
+                driver.switchTo().window(childWindow);
+                WebElement text = driver.findElement(By.id("sampleHeading"));
+                String actualText = text.getText();
+                String expectedText = "This is a sample page";
+                Assert.assertEquals(actualText, expectedText, "Text displayed is not correct");
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentWindow);
+    }
+
+    @Test
+    public void TC_020_verifyMultipleWindowHandlingHomeWork2() {
+        driver.get("https://demoqa.com/browser-windows");
+        String parentWindow = driver.getWindowHandle();
+        WebElement newWindowbutton = driver.findElement(By.id("windowButton"));
+        newWindowbutton.click();
+        Set<String> handles = driver.getWindowHandles();
+        Iterator<String> handleids = handles.iterator();
+        while (handleids.hasNext()) {
+            String childWindow = handleids.next();
+            if (! childWindow.equals(parentWindow)) {
+                driver.switchTo().window(childWindow);
+                WebElement text = driver.findElement(By.id("sampleHeading"));
+                String actualText = text.getText();
+                String expectedText = "This is a sample page";
+                Assert.assertEquals(actualText, expectedText, "Text displayed is not correct");
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentWindow);
+    }
+
+    @Test
+    public void TC_021_verifyMultipleWindowHandlingHomeWork3() {
+        driver.get("https://demoqa.com/browser-windows");
+        String parentWindow = driver.getWindowHandle();
+        WebElement windowMessageButton = driver.findElement(By.id("messageWindowButton"));
+        windowMessageButton.click();
+        Set<String> handles = driver.getWindowHandles();
+        Iterator<String> handleids = handles.iterator();
+        while (handleids.hasNext()) {
+            String childWindow = handleids.next();
+            if (! childWindow.equals(parentWindow)) {
+                driver.switchTo().window(childWindow);
+                WebElement text = driver.findElement(By.xpath("/html/body/text()"));
+                String actualText = text.getText();
+                String expectedText = "Knowledge increases by sharing but not by saving. Please share this website with your friends and in your organization.";
+                Assert.assertEquals(actualText, expectedText, "Text displayed is not correct");
+                driver.close();
+            }
+        }
+        driver.switchTo().window(parentWindow);
+    }
+    @Test
+    public void TC_022_verifySimpleAlert(){
+        driver.get("https://selenium.obsqurazone.com/javascript-alert.php");
+        WebElement clickMe= driver.findElement(By.xpath("//button[@class='btn btn-success']"));
+        clickMe.click();
+        Alert alert=driver.switchTo().alert();
+        String alertText = alert.getText();
+        System.out.println(alertText);
+        alert.accept();
+    }
+    @Test
+    public void TC_023_verifyConfirmationAlert(){
+        driver.get("https://selenium.obsqurazone.com/javascript-alert.php");
+        WebElement clickMe = driver.findElement(By.xpath("//button[@class='btn btn-warning']"));
+        clickMe.click();
+        Alert alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        System.out.println(alertText);
+        alert.dismiss();
+    }
+    @Test
+    public void TC_024_verifyPromptAlert(){
+        driver.get("https://selenium.obsqurazone.com/javascript-alert.php");
+        WebElement clickMe = driver.findElement(By.xpath("//button[@class='btn btn-danger']"));
+        clickMe.click();
+        Alert alert =driver.switchTo().alert();
+        String alertText =alert.getText();
+        System.out.println(alertText);
+        alert.sendKeys("SAM");
+        alert.accept();
+    }
+    @Test
+    public void TC_025_verifyTextInFrame(){
+        driver.get("https://demoqa.com/frames");
+        List<WebElement> frames=driver.findElements(By.tagName("iframe"));
+        int numberOfFrames= frames.size();
+        System.out.println(numberOfFrames);
+      //  driver.switchTo().frame("iframe");
+      //  driver.switchTo().frame("sampleHeading");
+        WebElement frame= driver.findElement(By.id("frame1"));
+        driver.switchTo().frame(frame);
+        WebElement heading = driver.findElement(By.id("sampleHeading"));
+        String text= heading.getText();
+        System.out.println(text);
+        driver.switchTo().parentFrame();
+      //  driver.switchTo().defaultContent();
+    }
+    @Test
+    public void TC_026_verifyRightClick(){
+        driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+        WebElement rightClick = driver.findElement(By.xpath("//span[@class='context-menu-one btn btn-neutral']"));
+        Actions action = new Actions(driver);
+        action.contextClick(rightClick).build().perform();
+        //action.contextClick().build().perform();
+    }
+    @Test
+    public void TC_027_verifyDoubleClick(){
+        driver.get("https://demo.guru99.com/test/simple_context_menu.html");
+        WebElement doubleClick= driver.findElement(By.xpath("//button[text()='Double-Click Me To See Alert']"));
+        Actions actions= new Actions(driver);
+        actions.doubleClick(doubleClick).build().perform();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+    }
+    @Test
+    public void TC_028_verifyMouseOver(){
+        driver.get("https://demoqa.com/menu/");
+        WebElement mainItem1= driver.findElement(By.xpath("//a[text()='Main Item 1']"));
+        Actions action = new Actions(driver);
+      // action.moveToElement(mainItem1).build().perform();
+      //  action.moveToElement(mainItem1,50,50).build().perform();
+        action.moveByOffset(40,80).build().perform();
+    }
+    @Test
+    public void TC_029_verifyDragAndDrop(){
+        driver.get("https://demoqa.com/droppable");
+        WebElement dragMe= driver.findElement(By.xpath("//div[@id='draggable']"));
+        WebElement dropHere= driver.findElement(By.xpath("//div[@id='droppable']"));
+        Actions action = new Actions(driver);
+        action.dragAndDrop(dragMe,dropHere).build().perform();
+
+    }
+    @Test
+    public void TC_030_verifyDragAndDropByOffset(){
+        driver.get("https://demoqa.com/dragabble");
+        WebElement drag= driver.findElement(By.xpath("//div[text()='Drag me']"));
+        Actions action = new Actions(driver);
+        action.dragAndDropBy(drag,100,100).build().perform();
+    }
+    @Test
+    public void TC_031_verifyDragAndResizeHW(){
+        driver.get("https://demoqa.com/resizable");
+        WebElement resize = driver.findElement(By.xpath("//*[@id=\"resizableBoxWithRestriction\"]/span"));
+        Actions action = new Actions(driver);
+        //action.clickAndHold(resize).moveByOffset(200,100).build().perform();
+        action.dragAndDropBy(resize,200,100).build().perform();
+    }
+    @Test
+    public void TC_032_verifyDragAndDropHW(){
+        driver.get("https://selenium.obsqurazone.com/drag-drop.php");
+        WebElement drag1 = driver.findElement(By.xpath("//span[text()='Draggable n°1']"));
+        WebElement drag2 = driver.findElement(By.xpath("//span[text()='Draggable n°2']"));
+        WebElement drag3=driver.findElement(By.xpath("//span[text()='Draggable n°3']"));
+        WebElement drag4 = driver.findElement(By.xpath("//span[text()='Draggable n°4']"));
+        WebElement drop = driver.findElement(By.xpath("//div[@dropzone='move']"));
+
+        Actions action = new Actions(driver);
+        //action.clickAndHold(drag).moveToElement(drop).release().build().perform();
+        action.dragAndDrop(drag1,drop).build().perform();
+        action.dragAndDrop(drag2,drop).build().perform();
+        action.dragAndDrop(drag3,drop).build().perform();
+        action.dragAndDrop(drag4,drop).build().perform();
+        //action.dragAndDropBy();
+    }
+    @Test
+    public void verifyValuesInDropDown(){
+        driver.get("https://demo.guru99.com/test/newtours/register.php");
+        List<String> expDropDownList = new ArrayList<>();
+        expDropDownList.add("ALBANIA");
+        expDropDownList.add("ALGERIA");
+        expDropDownList.add("AMERICAN SAMOA");
+        expDropDownList.add("ANDORRA");
+        List<String> actDropDownList = new ArrayList<>();
+        WebElement countryDropDown= driver.findElement(By.xpath("//select[@name='country']"));
+        Select select = new Select(countryDropDown);
+        List <WebElement> dropDownOption =select.getOptions();
+        System.out.println(dropDownOption.size());
+        for(int i=0; i<4;i++){
+            //System.out.println(dropDownOption.get(i).getText());
+            actDropDownList.add(dropDownOption.get(i).getText());
+        }
+        System.out.println(actDropDownList);
+        Assert.assertEquals(actDropDownList,expDropDownList,"DropDown values not found");
+        //select.selectByVisibleText("INDIA");
+        //select.selectByIndex(23);
+        select.selectByValue("INDIA");
+        select.getAllSelectedOptions();
+    }
+@Test
+    public void verifyMethodsInSelectClass(){
+        driver.get("https://www.softwaretestingmaterial.com/sample-webpage-to-automate/");
+        WebElement dropDown = driver.findElement(By.xpath("(//select[@class='spTextField'])[1]"));
+        Select select= new Select(dropDown);
+        boolean multipleStatus =select.isMultiple();
+        System.out.println(multipleStatus);
+        select.selectByVisibleText("Performance Testing");
+        select.selectByValue("msmanual");
+        List<WebElement> selectedOption= select.getAllSelectedOptions();
+        for(int i=0; i< selectedOption.size();i++){
+            System.out.println(selectedOption.get(i).getText());
+        }
+    select.deselectByValue("msmanual");
+    select.deselectAll();
+}
 }
